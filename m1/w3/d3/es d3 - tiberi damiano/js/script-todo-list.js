1158cdc2543e099c4b619c2e7c5be2254ed4bd8b
DamTib4638 <damiano.tiberi@gmail.com>
2022-11-09 20:57:40 +0100

    ToDo List Damiano

--- m1/w3/d3/es d3 - tiberi damiano/js/script-todo-list.js
+++ m1/w3/d3/es d3 - tiberi damiano/js/script-todo-list.js
@@ -1,47 +1,85 @@
-document.querySelector('#aggiungi')
-        .addEventListener('click', function(){
+let pulsanteAggiungi = document.querySelector('#aggiungi');
+pulsanteAggiungi.addEventListener('click', aggiungiCompito);
 
-            let testo = document.querySelector('#text');
+function aggiungiCompito() {
+
+    let testo = document.querySelector('#text');
+
+    if (testo.value != '') {
+    
+        let div = document.createElement('div');
+        div.classList.add('todo');
+        
+        let span = document.createElement('span');
+        span.textContent = testo.value.toUpperCase();
+        span.classList.add('span-compito');
+
+        let bottoneFatto = document.createElement('button');
+        bottoneFatto.textContent = '✓';
+        bottoneFatto.classList.add('button-fatto');
+
+        let bottoneElimina = document.createElement('button');
+        bottoneElimina.textContent = 'X';
+        bottoneElimina.classList.add('button-elimina');
+
+        let principale = document.querySelector('#principale');
+        
+        testo.value = '';
+
+        div.append(span, bottoneElimina, bottoneFatto);
+        
+        principale.append(div);
+
+        svolgiCompito(bottoneFatto, div);
+        eliminaCompito(bottoneElimina, div);  
+    } else {
+        alert ('Devi inserire una nuova attività per aggiungerla');
+    }
+}
+
+function eliminaCompito(bottoneElimina, div) {
+    bottoneElimina.addEventListener('click', function () {
+        let lineaDaEliminare = document.querySelector('#principale');
+        lineaDaEliminare.removeChild(div);
+    })
+}
+
+function svolgiCompito(bottoneFatto, div) {
+    bottoneFatto.addEventListener('click', function() {
+        let compitoSvolto = div;
+        if (compitoSvolto.style.backgroundColor != 'lightblue') {
+            compitoSvolto.style.backgroundColor = 'lightblue';
+            compitoSvolto.style.textDecoration = 'line-through';
+            let bottoneSost = compitoSvolto.querySelector('.button-fatto');
+            bottoneSost.style.backgroundColor = 'steelblue';
+            bottoneSost.textContent = '🠑';
+        } else {
+            compitoSvolto.style.backgroundColor = 'lightcoral';
+            compitoSvolto.style.textDecoration = 'none';
+            let bottoneSost = compitoSvolto.querySelector('.button-fatto');
+            bottoneSost.style.backgroundColor = 'green';
+            bottoneSost.textContent = '✓';
+        }
+        
+    })
+}
+
+
+
+        // let cerca = document.querySelector('#cerca');
+
+        // cerca.addEventListener('keyup', ()=> {
             
-            let div = document.createElement('div');
-            div.classList.add('todo')
+        //     let todos = document.querySelectorAll('.todo');
             
-            let span = document.createElement('span');
-            span.textContent = testo.value;
-            
-
-            let completato = document.createElement('button');
-            completato.textContent = '✓';
-
-            completato.addEventListener('click', ()=> {
-                div.classList.add('completato');//assegno una classe aggiuntiva
-                document.querySelector('#lista-completati')
-                .append(div);//sposto il div nella lista completati
-                completato.textContent = 'X';
-            })
-
-            div.append(span, completato);
-            document.querySelector('#lista').append(div);
-
-            testo.value = '';  
-        });
-
-
-
-        let cerca = document.querySelector('#cerca');
-
-        cerca.addEventListener('keyup', ()=> {
-            
-            let todos = document.querySelectorAll('.todo');
-            
-            for(let task of todos){
-                let testo = task.querySelector('span').textContent.toUpperCase();
+        //     for(let task of todos){
+        //         let testo = task.querySelector('span').textContent.toUpperCase();
                 
-                if(testo.search(cerca.value.toUpperCase()) == -1){
-                    task.style.display = 'none';
-                }else{
-                    task.style.display = 'block';
-                }
-            }
+        //         if(testo.search(cerca.value.toUpperCase()) == -1){
+        //             task.style.display = 'none';
+        //         }else{
+        //             task.style.display = 'block';
+        //         }
+        //     }
 
-        })
+        // })

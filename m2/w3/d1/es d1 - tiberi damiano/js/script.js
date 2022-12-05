4c260c04738aa988e730aae7bff14a5558b9d51a
DamTib4638 <damiano.tiberi@gmail.com>
2022-12-05 23:00:12 +0100

    es d1 w7 - tiberi damiano

--- m2/w3/d1/es d1 - tiberi damiano/js/script.js
+++ m2/w3/d1/es d1 - tiberi damiano/js/script.js
@@ -2,50 +2,47 @@ function Persona (nomeF, cognomeF, dataNascitaF) {
     this.nome = nomeF;
     this.cognome = cognomeF;
     this.dataNascita = dataNascitaF;
+    this.ricavaEta = function() {
+        if (this.dataNascita) {
+            let eta = 0;
+            let annoCorrente = new Date().getFullYear();
+            let annoUtente = new Date(this.dataNascita).getFullYear();
+            if (annoCorrente > annoUtente) {
+                eta = annoCorrente-annoUtente;
+                return eta;
+            } else {
+                alert('Devi ancora nascere');
+            }
+        } else {
+            alert('Inserisci la tua data di nascita');
+        }
+    }
+    this.aggiungiUtente = function() {
+        let tBody = document.getElementById('cuoreTabella');
+        let nuovaRiga = document.createElement('tr');
+        let nuovaColonnaNominativo = document.createElement('td');
+        let nuovaColonnaEta = document.createElement('td');
+        nuovaColonnaNominativo.classList.add('col-6');
+        nuovaColonnaNominativo.textContent += this.nome + ' ' + this.cognome;
+        nuovaColonnaEta.classList.add('col-6');
+        nuovaColonnaEta.textContent += this.ricavaEta();
+        nuovaRiga.appendChild(nuovaColonnaNominativo);
+        nuovaRiga.appendChild(nuovaColonnaEta);
+        tBody.appendChild(nuovaRiga);
+    }
 }
 
 let nomeForm = document.getElementById('nome');
 let cognomeForm = document.getElementById('cognome');
 let dataNascitaForm = document.getElementById('dataNascita');
 let salvaUtente = document.getElementById('salva');
-let tBody = document.getElementById('cuoreTabella');
-
-function calcoloEta (dataUtente) {
-    let eta = 0;
-    if (dataUtente) {
-        let annoUtente = new Date(dataUtente);
-        let annoCorrente = new Date();
-        annoCorrente = annoCorrente.getFullYear();
-        annoUtente = annoUtente.getFullYear();
-        console.log(annoCorrente);
-        console.log(annoUtente);
-        if (annoCorrente > annoUtente) {
-            eta = annoCorrente-annoUtente;
-        } else {
-            alert('Devi ancora nascere');
-        }
-    } else {
-        alert('Inserisci la tua data di nascita');
-    }
-    return eta;
-}
-
 salvaUtente.addEventListener('click', inserisciUtente);
 
-function inserisciUtente(e) {
+function inserisciUtente() {
     let nuovoUtente = new Persona(nomeForm.value, cognomeForm.value, dataNascitaForm.value);
+    svuotaCampi();
     if ((nuovoUtente.nome || nuovoUtente.cognome) && nuovoUtente.dataNascita) {
-        let nuovaRiga = document.createElement('tr');
-        let nuovaColonnaNominativo = document.createElement('td');
-        let nuovaColonnaEta = document.createElement('td');
-        nuovaColonnaNominativo.classList.add('col-6');
-        nuovaColonnaNominativo.textContent += nuovoUtente.nome + ' ' + nuovoUtente.cognome;
-        nuovaColonnaEta.classList.add('col-6');
-        nuovaColonnaEta.textContent += calcoloEta(nuovoUtente.dataNascita);
-        nuovaRiga.appendChild(nuovaColonnaNominativo);
-        nuovaRiga.appendChild(nuovaColonnaEta);
-        tBody.appendChild(nuovaRiga);
-        svuotaCampi();
+        nuovoUtente.aggiungiUtente();
     } else {
         alert('Inserisci bene i campi');
     }
